60f0b52d704555004265b80b49653dfdeb9976a9
Godfrey Chiu <godfrey.chiu@catenamedia.com>
2018-06-12 09:45:00 +0100

    Building our first website - added PagesController, methods and view files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+    <title>Home Page</title>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+</body>
+</html>
